269928ac86f51467cb6026817641a93f14ce2933
pyk <panyk@hdsxtech.com>
2015-10-19 13:57:28 +0800

    ...

--- src/com/weibo/util/WeiboTask.java
+++ src/com/weibo/util/WeiboTask.java
@@ -2,8 +2,11 @@ package com.weibo.util;
 
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import com.weibo.service.WeiboService;
 
 /**
  * 定时发送微博
@@ -12,8 +15,11 @@ import org.springframework.stereotype.Component;
 @Component
 @Scope("prototype")//每一个请求都有一个类来处理，避免线程安全问题。
 public class WeiboTask {
+	@Autowired
+	private WeiboService weiboService;
 	public void sendWeibo() {
 		System.out.println("发送微博开始：" + new Date());  
+		weiboService.doWeibo();
         System.out.println("发送微博结束：" + new Date());  
 	}
 }
