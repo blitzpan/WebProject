20366733fe36c05b1e183ab21862cc5a6fbf32ea
pyk <panyk@hdsxtech.com>
2015-11-05 09:59:54 +0800

    七牛正常运行

--- src/com/qiniu/Base64Coder.java
+++ src/com/qiniu/Base64Coder.java
@@ -6,10 +6,10 @@ import org.apache.commons.codec.binary.StringUtils;
 public class Base64Coder {
 
 	public static String decode(String s) {
-		return StringUtils.newStringUtf8(Base64.decodeBase64(s));
+		return StringUtils.newStringUtf8(Base64.decodeBase64(s.getBytes()));
 	}
 
 	public static String encode(String s) {
-		return Base64.encodeBase64String(StringUtils.getBytesUtf8(s));
+		return Base64.encodeBase64(StringUtils.getBytesUtf8(s)).toString();
 	}
 }
